a9e638489fcdab5a0c9fd4cafc5408aa67706eb2
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-06 13:10:44 -0700

    Update 5. Meeting Rooms II [LintCode #919].py

--- Submits/Intervals/5. Meeting Rooms II [LintCode #919].py
+++ Submits/Intervals/5. Meeting Rooms II [LintCode #919].py
@@ -41,3 +41,48 @@ Space Complexity - O(n)
 
 # Grokking Solution 
 
+from heapq import *
+
+class Meeting:
+  def __init__(self, start, end):
+      self.start = start
+      self.end = end
+
+  def __lt__(self, other):
+      # min heap based on meeting.end
+      return self.end < other.end
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
+        # Write your code here
+        intervals.sort(key=lambda x: x.start)
+
+        # Converting to own list of meetings to use function __lt__
+        meetings = []
+        for i in intervals:
+            meetings.append(Meeting(i.start, i.end))
+
+        minRooms = 0
+        minHeap = []
+
+        for meeting in meetings:
+            # remove all the meetings that have ended
+            while (len(minHeap)) > 0 and meeting.start >= minHeap[0].end:
+                heappop(minHeap)
+
+            # add the current meeting into min_heap
+            heappush(minHeap, meeting)
+            # all active meetings are in the min_heap, so we need rooms for all of them 
+            minRooms = max(minRooms, len(minHeap))
+
+        return minRooms
+
+'''
+Time Complexity - O(nlogn) for sorting
+Space Complexity - O(n)
+'''
