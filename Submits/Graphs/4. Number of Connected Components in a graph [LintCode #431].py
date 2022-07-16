5e4df6d488fac98655d477dd9bba930fe4646a3c
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-16 12:44:23 -0700

    Create 4. Number of Connected Components in a graph [LintCode #431].py

---
+++ Submits/Graphs/4. Number of Connected Components in a graph [LintCode #431].py
@@ -0,0 +1,52 @@
+# LintCode Link - LintCode Premium
+
+from typing import (
+    List,
+)
+from lintcode import (
+    UndirectedGraphNode,
+)
+
+"""
+class UndirectedGraphNode:
+     def __init__(self, x):
+         self.label = x
+         self.neighbors = []
+"""
+
+class Solution:
+    """
+    @param nodes: a array of Undirected graph node
+    @return: a connected set of a Undirected graph
+    """
+    def connectedSet(self, nodes: List[UndirectedGraphNode]) -> List[List[int]]:
+        # write your code here
+
+        result_list = []
+        visited = {i.label:False for i in nodes}
+
+        def dfs(i, connected_list):
+
+            visited[i.label] = True
+            connected_list.append(i.label)
+
+            for j in i.neighbors:
+                if not visited[j.label]:
+                    dfs(j, connected_list)
+
+            i.neighbors = [] # This line is super-critical to reduce time Complexity
+            return connected_list
+
+        for i in nodes:
+            if not visited[i.label]:
+                result = sorted(dfs(i, []))
+                if result: result_list.append(result)
+
+        return result_list
+
+    
+                
+'''
+Time Complexity - O(|V|+|E|)
+Space Complexrity - O(|V|)
+'''
