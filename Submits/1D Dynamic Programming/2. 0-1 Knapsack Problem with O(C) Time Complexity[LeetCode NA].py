3890faccbe45b33883f4c10a298059ad057289aa
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-23 18:57:18 -0700

    Update 2. 0-1 Knapsack Problem with O(C) Time Complexity[LeetCode NA].py

--- Submits/1D Dynamic Programming/2. 0-1 Knapsack Problem with O(C) Time Complexity[LeetCode NA].py
+++ Submits/1D Dynamic Programming/2. 0-1 Knapsack Problem with O(C) Time Complexity[LeetCode NA].py
@@ -4,29 +4,26 @@ def solve_knapsack(profits, weights, capacity):
     if capacity <= 0 or n == 0 or len(weights) != n:
         return 0
 
-    # we only need one previous row to find the optimal solution, overall we need '2' rows
-    # the above solution is similar to the previous solution, the only difference is that
-    # we use `i % 2` instead if `i` and `(i-1) % 2` instead if `i-1`
-    dp = [[0 for x in range(capacity+1)] for y in range(2)]
+    dp = [0 for x in range(capacity+1)]
 
     # if we have only one weight, we will take it if it is not more than the capacity
     for c in range(0, capacity+1):
         if weights[0] <= c:
-            dp[0][c] = dp[1][c] = profits[0]
+            dp[c] = profits[0]
 
     # process all sub-arrays for all the capacities
     for i in range(1, n):
-        for c in range(0, capacity+1):
+        for c in range(capacity, -1, -1):
             profit1, profit2 = 0, 0
             # include the item, if it is not more than the capacity
             if weights[i] <= c:
-                profit1 = profits[i] + dp[(i - 1) % 2][c - weights[i]]
+                profit1 = profits[i] + dp[c - weights[i]]
             # exclude the item
-            profit2 = dp[(i - 1) % 2][c]
+            profit2 = dp[c]
             # take maximum
-            dp[i % 2][c] = max(profit1, profit2)
+            dp[c] = max(profit1, profit2)
 
-    return dp[(n - 1) % 2][capacity]
+    return dp[capacity]
 
 
 def main():
@@ -37,6 +34,7 @@ def main():
 
 
 main()
+
 
 '''
 Time Complexity - O(N.C)
