43fa4cfdb881a4a21207f276aa88085449fe98ee
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-27 13:58:55 -0700

    Create 5. Decode Ways [LeetCode #91].py

---
+++ Submits/1D Dynamic Programming/5. Decode Ways [LeetCode #91].py
@@ -0,0 +1,22 @@
+# LeetCode Link - https://leetcode.com/problems/decode-ways/
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = { len(s) : 1 } # for empty string and string of len 1 we want to return 1
+        
+        for i in range(len(s)-1, -1, -1):
+            
+            if s[i] == "0":
+                dp[i] = 0 # because string cannot start with 0
+            else:
+                dp[i] = dp[i+1] # for length of first substring 1
+
+                if (i+1< len(s) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456"))): # for length of first substring 2
+                    dp[i] += dp[i+2]
+        
+        return dp[0]
+
+'''
+Time Complexity - O(n)
+Space Complexity - O(1)
+'''
