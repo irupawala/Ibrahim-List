ae8138562395cf724a2f3c2b222a72abb332d4e4
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-28 16:11:15 -0700

    Create 7. Longest Increasing Subsequence [LeetCode #300].py

---
+++ Submits/1D Dynamic Programming/7. Longest Increasing Subsequence [LeetCode #300].py
@@ -0,0 +1,17 @@
+# LeetCode Link - https://leetcode.com/problems/longest-increasing-subsequence/
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+        
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], LIS[j]+1)
+        
+        return max(LIS)
+    
+'''
+Time Complexity - O(n*n)
+Space Complexity - O(n)
+'''
