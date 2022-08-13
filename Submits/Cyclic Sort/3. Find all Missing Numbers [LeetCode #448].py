7fe429ba270e4adf5e7601027a2f5fd7a372c06c
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-08-13 13:32:10 -0700

    Create 3. Find all Missing Numbers [LeetCode #448].py

---
+++ Submits/Cyclic Sort/3. Find all Missing Numbers [LeetCode #448].py
@@ -0,0 +1,22 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        i = 0
+        while i < len(nums):
+            j = nums[i]-1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        
+        missingNumbers = []
+        
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                missingNumbers.append(i+1)
+                
+        return missingNumbers
+                
+'''
+Time Complexity - O(N)
+Space Complexity - O(1)
+'''
