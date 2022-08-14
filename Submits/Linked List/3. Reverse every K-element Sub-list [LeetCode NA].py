3f4b2293d2a7f3c287853e52bdb5f3da0dc61b0c
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-08-13 19:55:03 -0700

    Update 3. Reverse every K-element Sub-list [LeetCode NA].py

--- Submits/Linked List/3. Reverse every K-element Sub-list [LeetCode NA].py
+++ Submits/Linked List/3. Reverse every K-element Sub-list [LeetCode NA].py
@@ -47,3 +47,8 @@ head.next.next = Node(3)
 head.next.next.next = Node(4)
 head.next.next.next.next = Node(5)
 print(S.reverseKGroup(head, 3))
+
+'''
+Time Complexity - O(N)
+Space Complexity - O(1)
+'''
