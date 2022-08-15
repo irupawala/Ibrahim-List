4d942a0f7fcdda2cd59aae52e3bf204f33d9a024
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-08-14 20:38:07 -0700

    Update 3. Group Anagrams [LeetCode #49].py

--- Submits/Array & Hashings/3. Group Anagrams [LeetCode #49].py
+++ Submits/Array & Hashings/3. Group Anagrams [LeetCode #49].py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) == 1: return [strs] 
@@ -9,8 +10,29 @@ class Solution:
             else: ans[dict_key].append(i)
                 
         return ans.values()
-    
+'''   
+
 '''
-Time Complexity - O(len(n) * mlogm)
-Space Complexity - O(len(n))
+Time Complexity - O(len(strs) * mlogm)
+Space Complexity - O(len(strs))
+'''
+
+
+# Linear time algorithm by Neetcode
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+
+
+'''
+Time Complexity - O(len(str) * m * 26)
+Space Complexity - O(len(str) * m)
 '''
