2ed108bc53884258a36bfc44e598bf8cc063948e
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-08-16 14:17:17 -0700

    Create 2. Subsets With Duplicates [LeetCode #90].py

---
+++ Submits/Subsets/2. Subsets With Duplicates [LeetCode #90].py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        n = len(nums)
+        res = [[]]
+        last_subset = []
+        
+        for counter in range(n):
+            startIndex = 0
+            if counter > 0 and nums[counter] == nums[counter-1]:
+                startIndex = endIndex 
+            endIndex = len(res)
+            for i in range(startIndex, endIndex):
+                set1 = res[i][:]
+                set1.append(nums[counter])
+                res.append(set1)
+                last_subset.append(set1) 
+        
+        return res
+
+'''
+Time Complexity - O(N.2^N)
+Space Complexity - O(N.2^N)
+'''
