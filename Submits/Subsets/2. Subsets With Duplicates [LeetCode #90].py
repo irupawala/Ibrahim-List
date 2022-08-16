e111d842d1f80ad89945a79be1ef84b8dc3121c7
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-08-16 15:46:57 -0700

    Update 2. Subsets With Duplicates [LeetCode #90].py

--- Submits/Subsets/2. Subsets With Duplicates [LeetCode #90].py
+++ Submits/Subsets/2. Subsets With Duplicates [LeetCode #90].py
@@ -1,3 +1,32 @@
+# BFS Solution
+'''
+class Solution:
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        n = len(nums)
+        res = [[]]
+        last_subset = []
+        
+        for counter in range(n):
+            startIndex = 0
+            if counter > 0 and nums[counter] == nums[counter-1]:
+                startIndex = endIndex 
+            endIndex = len(res)
+            for i in range(startIndex, endIndex):
+                set1 = res[i][:]
+                set1.append(nums[counter])
+                res.append(set1)
+                last_subset.append(set1) 
+        
+        return res
+'''        
+        
+# DFS Solution
+'''
+Time Complexity - O(N.2^N)
+Space Complexity - O(N.2^N)
+'''
+
 class Solution:
     def subsetsWithDup(self, nums):
         nums.sort()
