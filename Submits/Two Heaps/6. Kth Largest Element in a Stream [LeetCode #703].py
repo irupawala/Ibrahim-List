5e63f041fb45c96e490dbfbcac63443617163a34
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-08-25 10:08:43 -0700

    Create 6. Kth Largest Element in a Stream [LeetCode #703].py

---
+++ Submits/Two Heaps/6. Kth Largest Element in a Stream [LeetCode #703].py
@@ -0,0 +1,21 @@
+from heapq import *
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+        # minHeap w/ K largest integers
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+    
+'''
+Time Complexity - heap - klog(k), add - O(logk)
+Space Complexity - O(k)
+'''
