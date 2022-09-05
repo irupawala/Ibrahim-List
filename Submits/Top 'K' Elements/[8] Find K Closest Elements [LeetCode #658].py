07059f3f4fb027ec311e040c1f0d5cb772fdb607
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-09-05 11:14:27 -0700

    Create [8] Find K Closest Elements [LeetCode #658].py

---
+++ Submits/Top 'K' Elements/[8] Find K Closest Elements [LeetCode #658].py
@@ -0,0 +1,42 @@
+from heapq import *
+
+class Solution:
+    def findClosestElements(self, arr, k, x):
+        index = self.binary_search(arr, x)
+        low, high = index - k, index + k
+
+        low = max(low, 0)  # 'low' should not be less than zero
+        # 'high' should not be greater the size of the array
+        high = min(high, len(arr) - 1)
+
+        minHeap = []
+        # add all candidate elements to the min heap, sorted by their absolute difference from 'X'
+        for i in range(low, high+1):
+            heappush(minHeap, (abs(arr[i] - x), arr[i]))
+
+        # we need the top 'K' elements having smallest difference from 'X'
+        result = []
+        for _ in range(k):
+            result.append(heappop(minHeap)[1])
+
+        result.sort()
+        return result        
+    
+    def binary_search(self, arr,  target):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low) / 2)
+            if arr[mid] == target:
+                return mid
+            if arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        if low > 0:
+            return low - 1
+        return low    
+    
+'''
+Time Complexity - O(logN+K∗logK)
+Space Complexity - O(K∗logK)
+'''
